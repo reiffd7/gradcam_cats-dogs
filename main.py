3d1e5f1e785217d7e29e66928330a18800e22b77
Daniel Reiff <reiffd@wusl.edu>
2021-07-19 13:57:05 -0400

    aug model with more imgs

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ if __name__ == '__main__':
     IMG_SIZE = (224, 224, 3)
     TEST_SIZE = 0.2
     RANDOM_STATE = 2018
-    data_dir = DATA_PATH
+    data_dir = AUG_PATH
     X, y, files = manual_pre_process(data_dir, 224, DATA_SAMPLE_SIZE)
     # indeces = np.arange(DATA_SAMPLE_SIZE + AUG_SAMPLE_SIZE)
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, stratify=y, random_state=RANDOM_STATE)
-    aug_samples = [0, 0.25, 0.50, 0.75, 1.0]
+    aug_samples = [0]
     for sample in aug_samples:
         AUG_SAMPLE_SIZE = 992 * sample
         X_aug, y_aug, files_aug = manual_pre_process(AUG_PATH, 224, AUG_SAMPLE_SIZE)
@@ -152,10 +152,11 @@ if __name__ == '__main__':
         activation_layers = [layer.name for layer in reversed(model.layers) if len(layer.output_shape) == 4 and layer.__class__.__name__ == 'ReLU']
         all_layers = [layer.name for layer in reversed(model.layers) if len(layer.output_shape) == 4 and (layer.__class__.__name__ == 'ReLU' or isinstance(layer, tf.keras.layers.Conv2D))]
 
-
-        img_path = '../cats-dogs-data/cat_cage_7.jpeg'
+        img_paths = os.listdir('../cats-dogs-data/Cats&DogsTransporters')
+        for img_path in img_paths:
+        # img_path = '../cats-dogs-data/cat_cage_7.jpeg'
         
-        model_type = AUG_SAMPLE_SIZE
-        
+            model_type = AUG_SAMPLE_SIZE
+            
 
-        predict_and_interpret(img_path, model, DATA_SAMPLE_SIZE, model_type, all_layers)
+            predict_and_interpret(img_path, model, DATA_SAMPLE_SIZE, model_type, all_layers)
